82dbe3be9162627a3fb3e28c951ab936192d4553
Shwetha-Iyer <shwetharaj555@gmail.com>
2021-04-05 16:27:53 +0530

    Update pages.js

--- pages.js
+++ pages.js
@@ -78,86 +78,27 @@ request.onload =function() {
     liprev.addEventListener("click", display);
     aprev.innerHTML = "Previous";
     liprev.append(aprev);
+    ul.append(liprev);
+
+    var j=1;
+    while(j<=10){
+        var li = createTag("li","page-item");
+        var a = createTag("a","page-link");
+        a.setAttribute("id",j);
+        a.addEventListener("click",display);
+        a.innerHTML = j;
+        li.append(a);
+        ul.append(li);
+        j++;
+    }
     
-    var li1 = createTag("li","page-item");
-    var a1 = createTag("a","page-link");
-    a1.setAttribute("id","1");
-    a1.addEventListener("click", display);
-    a1.innerHTML = "1";
-    li1.append(a1);
-
-    var li2 = createTag("li","page-item");
-    var a2 = createTag("a","page-link");
-    a2.setAttribute("id","2");
-    a2.addEventListener("click", display);
-    a2.innerHTML = "2";
-    li2.append(a2);
-
-    var li3 = createTag("li","page-item");
-    var a3 = createTag("a","page-link");
-    a3.setAttribute("id","3");
-    a3.addEventListener("click", display);
-    a3.innerHTML = "3";
-    li3.append(a3);
-
-    var li4 = createTag("li","page-item");
-    var a4 = createTag("a","page-link");
-    a4.setAttribute("id","4");
-    a4.addEventListener("click", display);
-    a4.innerHTML = "4";
-    li4.append(a4);
-
-    var li5 = createTag("li","page-item");
-    var a5 = createTag("a","page-link");
-    a5.setAttribute("id","5");
-    a5.addEventListener("click", display);
-    a5.innerHTML = "5";
-    li5.append(a5);
-
-    var li6 = createTag("li","page-item");
-    var a6 = createTag("a","page-link");
-    a6.setAttribute("id","6");
-    a6.addEventListener("click", display);
-    a6.innerHTML = "6";
-    li6.append(a6);
-
-    var li7 = createTag("li","page-item");
-    var a7 = createTag("a","page-link");
-    a7.setAttribute("id","7");
-    a7.addEventListener("click", display);
-    a7.innerHTML = "7";
-    li7.append(a7);
-
-    var li8 = createTag("li","page-item");
-    var a8 = createTag("a","page-link");
-    a8.setAttribute("id","8");
-    a8.addEventListener("click", display);
-    a8.innerHTML = "8";
-    li8.append(a8);
-
-    var li9 = createTag("li","page-item");
-    var a9 = createTag("a","page-link");
-    a9.setAttribute("id","9");
-    a9.addEventListener("click", display);
-    a9.innerHTML = "9";
-    li9.append(a9);
-
-    var li10 = createTag("li","page-item");
-    var a10 = createTag("a","page-link");
-    a10.setAttribute("id","10");
-    a10.addEventListener("click", display);
-    a10.innerHTML = "10";
-    li10.append(a10);
-
-
-
     var linext = createTag("li","page-item");
     var anext = createTag("a","page-link");
     anext.setAttribute("id","Next");
     linext.addEventListener("click", display);
     anext.innerHTML = "Next";
     linext.append(anext);
-    ul.append(liprev,li1,li2,li3,li4,li5,li6,li7,li8,li9,li10,linext);
+    ul.append(linext);
     nav.append(ul);
 
     table.append(thead,tbody);
@@ -165,6 +106,4 @@ request.onload =function() {
     row.append(col);
     container.append(row);
     document.body.append(h1,container);
-
-
 }
